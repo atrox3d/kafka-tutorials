5111b597f7fce8572632f1f776593fb5e0158410
atrox3d <atrox3d@gmail.com>
2025-08-24 15:58:38 +0000

    add orders backend

---
+++ src/event_driven_food_ordering_app/orders_backend.py
@@ -0,0 +1,36 @@
+import json
+from math import prod
+import time
+import os
+import random
+
+from kafka import KafkaProducer
+
+ORDER_KAFKA_TOPIC = "order_details"
+ORDER_LIMIT = 250
+ORDER_START_SECONDS = 5
+ORDER_WAIT_SECONDS = 1
+KAFKA_INTERNAL_PORT = os.environ.get("KAFKA_INTERNAL_PORT", "9092")
+KAFKA_BROKER_URL = f'kafka:{KAFKA_INTERNAL_PORT}'
+PRODUCER_CLIENT_ID = 'fastapi-producer'
+
+
+producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER_URL])
+
+print(f'Generating orders after {ORDER_START_SECONDS} seconds')
+print(f'Will generate one unique order every {ORDER_WAIT_SECONDS} seconds')
+time.sleep(ORDER_START_SECONDS)
+
+for i in range(ORDER_LIMIT):
+    data = {
+        "order_id": i,
+        "user_id": f"user_{i}",
+        "total_cost": i *  5,
+        "items": random.choice('hamburger salad hotdog sandwich'.split())
+    }
+
+    producer.send(ORDER_KAFKA_TOPIC, json.dumps(data).encode("utf-8"))
+    print(f'order sent: {data}')
+    time.sleep(ORDER_WAIT_SECONDS)
+
+print('All orders sent')
